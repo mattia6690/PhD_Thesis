e823ffda098884b9f7fdf0b315cecb132b55a094
Rossi Mattia <mattia.rossi@eurac.edu>
2018-04-18 10:13:25 +0200

    S2 Automatic Overflight forecast added

---
+++ R/04_Other/AcquisitionTimes S2.R
@@ -0,0 +1,128 @@
+
+source("R/BaseFunctions.R")
+library(rvest)
+library(httr)
+library(stringr)
+library(sf)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(tibble)
+library(Rcpp)
+#devtools::install_github("tidyverse/ggplot2")   # You need the newest possible ggplot version (>2.2.1) with geom_sf() function
+library(ggplot2)
+
+tempdir<-"C:/Users/MRossi/Documents/08_Temp/"
+outname<-paste0(tempdir,"S2_Tiles_lac.shp")
+inter2<-st_read(outname)
+
+outline<-inter2 %>% group_by(Inside_AC) %>% summarize()
+
+s2kmltable<-function(kmlfile){
+  
+  rl<-readLines(kmlfile)
+  re1 <- "\t\t\t\t\t<name>" %>% grep(.,rl)
+  
+  start<-re1[-length(re1)]
+  end<-re1[-1]
+  
+  test<-map2(start,end,function(i,j){
+    
+    re2<-rl[i:j]
+    re2rep<-str_replace_all(re2,"\t\t\t\t\t","")
+    
+    str2<-"<Data name" %>% grep(.,re2rep) %>% re2rep[.]
+    names<-str_split(str2,pattern='"') %>% lapply(.,function(i) i[2]) %>% unlist
+    
+    str3<-"<value" %>% grep(.,re2rep) %>% re2rep[.]
+    values <- str_match(str3, ">(.*?)<")[,2]
+    
+    dings<-rbind(names,values)
+    return(dings)
+    
+  })
+  
+  namesall<-lapply(test,function(i) length(i[1,])) %>% unlist %>% unique
+  
+  if(length(namesall)==1){
+    
+    namesfinal<-test[[1]][1,]
+    kmltable<-test %>% lapply(function(i) i[2,]) %>% 
+      do.call(rbind,.) %>% 
+      as.tibble %>% 
+      setNames(namesfinal)
+    return(kmltable)
+  }else warning("Uneven rows")
+  
+}
+
+# Harvest Data From Server
+starturl<-"https://sentinel.esa.int"
+url<-paste0(starturl,"/web/sentinel/missions/sentinel-2/acquisition-plans")
+webpage <- read_html(url)
+
+names<-c('div.sentinel-2a','div.sentinel-2b')
+
+urls<-map(names,function(i){
+  
+  titleA<-html_nodes(webpage,i)
+  matched <- str_match_all(titleA, "<a href=\"(.*?)\"")[[1]][1:2,2]
+  urls<-paste0(starturl,matched)
+  
+}) %>% unlist
+
+satellite<- urls %>% 
+  unlist %>% 
+  basename %>% 
+  map(.,function(i) str_split(i,pattern="_") %>% unlist %>% .[1]) %>% 
+  unlist
+
+maps<-map2(urls,satellite,function(urls,sa){
+  
+  name<- basename(urls)
+  file<- paste0(tempdir,name)
+  if(!file.exists(file)) download.file(urls,file)
+  sr<-st_read(file)
+  
+  table<-s2kmltable(file) %>% 
+    mutate(Name=ID)
+  inter3<-st_intersection(inter2,sr)
+  inter3$Satellite=sa
+  lj<-left_join(inter3,table,by="Name") %>% 
+    select(Satellite,Tile,ObservationTimeStart,ObservationTimeStop,Scenes) %>% 
+    arrange(ObservationTimeStart)
+})
+
+lj2<-do.call(rbind,maps)
+lj3<-lj2 %>% 
+  arrange(ObservationTimeStart) %>% 
+  mutate(Satellite2=as.numeric(ifelse(Satellite=="Sentinel-2B",2,1)))
+
+
+from<-as_date(lj3$ObservationTimeStart) %>% min
+to<-as_date(lj3$ObservationTimeStart) %>% max
+
+fileout <- paste0(sentineldir,"AcquisitionPlan_",from,"_to_",to)
+ggtitle <- paste("Sentinel 2 Acquisition Plan",from,"to",to)
+
+ggplot()+
+  geom_sf(data=outline)+
+  geom_sf(data=lj3,aes(fill=Satellite))+
+  facet_wrap(~ObservationTimeStart,ncol=7)+
+  theme(panel.grid.major.x = element_line(color = "grey", linetype = 2))+
+  ggtitle(ggtitle)
+
+ggsave(paste0(fileout,".png"), plot = last_plot(),device="png",width=14,height=10)
+
+tilefreq<-table(lj2$Tile)
+ljtile<-lj3 %>% 
+  select(ObservationTimeStart,Satellite,Tile) %>% 
+  distinct
+
+table<-ljtile %>% 
+  as.data.frame() %>% 
+  select(-geometry) %>% 
+  separate(.,ObservationTimeStart,c("Date","Time"),sep="T")
+
+write.csv(table,paste0(fileout,".csv"))
+saveRDS(table,paste0(fileout,"rds"))
---
+++ R/04_Other/AlpineTiles_S2.R
@@ -0,0 +1,27 @@
+
+library(sf)
+library(dplyr)
+library(magrittr)
+
+tempdir<-"C:/Users/MRossi/Documents/08_Temp/"
+
+lac<-st_read("C:/Users/MRossi/Documents/03_Data/Shapes/00_General","AC-perimeter_ifuplan_16072007")
+tilefile<-paste0(tempdir,"S2_Tiles.kml")
+Tiles<-st_read(tilefile)
+lac2<-st_transform(lac,as.character(st_crs(Tiles)[2]))
+Tiles2<-st_bind_cols(Tile=Tiles[[1]],Tiles[[3]])
+inter1<-st_intersection(lac2,Tiles2)
+inter2<-st_cast(inter1,"POLYGON")
+inter2<-st_zm(inter2)
+
+outname<-paste0(tempdir,"S2_Tiles_lac.shp")
+st_write(inter2,outname,driver = "ESRI Shapefile")
+
+colfunc<-colorRampPalette(c("red","yellow","springgreen","royalblue"))
+tilesplot<-inter2 %>% select(Tile) %>% arrange(Tile)
+plot(tilesplot,axes=T,
+     col = colfunc(length(tilesplot$Tile)), 
+     key.pos = 1,
+     main="Sentinel 2 Tiles in the Large Alpine Convention")
+
+
