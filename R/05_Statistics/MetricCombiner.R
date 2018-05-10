73417bd7809fc2c7b2d97d074eec4b08a37a7be6
Rossi Mattia <mattia.rossi@eurac.edu>
2018-05-10 11:19:48 +0200

    Added all the Management Analysis IGARSS

--- R/05_Statistics/MetricCombiner.R
+++ R/05_Statistics/MetricCombiner.R
@@ -24,7 +24,8 @@ monalisa_raw<- monalisa_raw %>% filter(Value!="NaN") %>%
 phenodat<-readRDS(paste0(PhenocamDir,"phenodat2.rds"))
 
 # Managements
-Manage<-readRDS(file = paste0(DataDir,"/Management/",station,"_Management_2017.rds"))
+Manage<-readRDS(file = paste0(DataDir,"/Management/",station,"_Management_2017.rds")) %>% 
+  dplyr::filter(Station==station)
 
 #* 2.1. Select the Data ----
 
@@ -39,10 +40,11 @@ s2<-sentinel2_raw %>%
   mutate(Value=Value/10000)
 
 groundN<-insitu_raw %>% 
-  filter(Station=="Vimes1500") %>% 
+  filter(Station==simpleCap(station)) %>% 
   filter(OP2=="NDVI") %>% 
-  group_by(Date,OP2) %>% summarise(Value=mean(Value)) %>% ungroup
-groundN$Date<-groundN$Date %>% as.Date(format="%d%m%y")
+  mutate(Date=as.Date(Date,format="%d%m%y")) %>% 
+  group_by(Date,OP2) %>%
+  dplyr::summarise(.,Value=mean(Value))
 
 phenodat<-phenodat %>% setNames(.,c("Date","Value"))
 
@@ -53,16 +55,18 @@ ggplot(groundN,aes(Date,Value))+
   ylab("NDVI")+
   ylim(c(.5,1))
 
-m.ndvi<-mnls %>% select(Date,Value) %>% setNames(c("Date","Monalisa"))
-s.ndvi<-s2 %>% select(Date,Value) %>% setNames(c("Date","Sentinel"))
+m.ndvi<-mnls %>% select(Date,Value) %>% setNames(c("Date","SRS"))
+s.ndvi<-s2 %>% select(Date,Value) %>% setNames(c("Date","Sentinel2"))
 p.ndvi<-phenodat %>% select(Date,Value) %>% setNames(c("Date","Phenocam"))
-g.ndvi<-groundN %>% select(Date,Value) %>% setNames(c("Date","Ground"))
+g.ndvi<-groundN %>% select(Date,Value) %>% setNames(c("Date","Spectrometer"))
 
 ndvi.tab<-full_join(m.ndvi,s.ndvi) %>% 
   full_join(.,p.ndvi) %>% 
   full_join(.,g.ndvi) %>% 
   add_column(Period=3) %>% 
   arrange(Date) 
+
+Harvest<-Manage %>% filter(Value=="Harvest")
 
 ndvi.tab$Period[which(ndvi.tab$Date<Harvest$Date[2])]<-2
 ndvi.tab$Period[which(ndvi.tab$Date<Harvest$Date[1])]<-1
@@ -72,7 +76,7 @@ saveRDS(ndvi.tab,file = paste0(MetricsDir,"NDVI_Table_vimes1500_2017.rds"))
 
 # 3. Plots of Metrics ----
 
-Harvest<-Manage %>% filter(Value=="Harvest")
+
 ggplot(phenodat,aes(Date,NDVI2))+ geom_point()
 
 
@@ -105,6 +109,23 @@ g2<-ggplot(mnls,aes(Date,Value,color="Decagon"))+theme_light()+ylab("NDVI")+
 
 ggsave(g1,file=paste0(dir,"Combined_NDVI_",station,".png"),device="png",width=10,height=7)
 ggsave(g2,file=paste0(dir,"Combined_NDVI_Management_",station,".png"),device="png",width=10,height=6)
+
+# Facet grid for pubblication
+
+ndvi.tab2<-ndvi.tab %>% 
+  distinct %>% 
+  setNames(c("Date","Decagon SRS","Sentinel-2 MSI","Phenocam","Spectrometer","Growing Period")) %>% 
+  gather(key=Sensor,value= NDVI,"Decagon SRS","Sentinel-2 MSI","Phenocam","Spectrometer")
+  
+g.fac<-ggplot(ndvi.tab2,aes(Date,NDVI))+ theme_bw()+
+  facet_wrap(.~Sensor)+
+  geom_point()+
+  geom_vline(data=Harvest2,aes(xintercept=Date),linetype="dashed")+
+  theme(axis.text.x = element_text(angle = 30, hjust = 1))+
+  ylim(c(0,1))+
+  scale_x_date(date_breaks = "2 month")
+
+ggsave(g.fac,file=paste0(dir,"Combined_NDVI_",station,"FacetGrid.png"),device="png",width=10,height=7)
 
 #* 3.2. NDVI & Management ----
 
@@ -142,20 +163,30 @@ for(i in 1:length(dsets)){
 }
 
 # IGARSS2018
-g1<-ggplot(dsets[[i]],aes(Date,Value))+theme_light()+ylab("NDVI")+
+
+sensor<-"SRS"
+ManageDiff<-Manage[-c(13,14),]
+igarssset<-ndvi.tab %>% select(Date,eval(sensor)) %>% mutate(Value=SRS)
+
+g1<-ggplot(igarssset,aes(Date,Value))+theme_light()+ylab("NDVI")+
   geom_point(aes(color="NDVI"),shape=0)+
-  geom_vline(data=Manage2,aes(xintercept=Date,linetype=Value))+
-  scale_linetype_manual("",values=c("Fertilization"="solid",
-                                              "Harvest"="longdash",
-                                              "Hay"="1F",
-                                              "Irrigation"="dotted",
-                                              "Livestock"="dotdash"))+
+  geom_vline(data=ManageDiff,aes(xintercept=Date,linetype=Value))+
+  scale_linetype_manual("",values=c("Fertilization"="dotted",
+                                    "Harvest"="solid",
+                                    "Irrigation"="dotdash",
+                                    "Livestock"="longdash"))+
   scale_color_manual("",values = c("NDVI"="darkgreen"))+
-  scale_x_date(limits=c(as.Date("2017-03-15"),as.Date("2017-10-15")))+
+  scale_x_date(limits=c(as.Date("2017-03-15"),as.Date("2017-10-31")),date_breaks="1 month")+
   ylim(0,1)+
   theme(legend.position="bottom",legend.text=element_text(size=12),axis.text=element_text(size=12))
+
+g1<- ggplot(igarssset,aes(Date,Value))+theme_light()+ylab("NDVI")+
+  geom_point(aes(color="NDVI"),shape=0)+
+  facet_wrap(~.ManageDiff)
+
   
-ggsave(g1,filename = paste0(dir,"NDVI_",names[i],"_IGARSSManagement_",station,".png"),device="png",width=9,height=7)
+ggsave(g1,filename = paste0(dir,"NDVI_",sensor,"_IGARSSManagement_",station,".png"),
+       device="png",width=9,height=7)
   
 #* 3.3. Scale Correlations ----
 
@@ -413,33 +444,270 @@ ggplot(diffs,aes(x=Date,y=Value))+
   geom_point(data=groundN,aes(Date,Value))
 
 
-# Look at management
+### 4. Management ----
 
-ManageSub<-Manage[11,]
+data<-ndvi.tab %>% 
+  distinct %>% 
+  gather(key="Sensor",value=Value,SRS,Sentinel2,Phenocam,Spectrometer,-Period) %>% 
+  select(-Period) 
 
-lista<-list()
-for(i in 14:1){
+ndays<-30
+sensorsall<-data$Sensor %>% unique
+
+mvallist<-list()
+for(s in 1:length(sensors)){
   
-  ManageSubDateB<-ManageSub$Date-i
-  ManageSubDate<-ManageSub$Date
-  ManageSubDateA<-ManageSub$Date+i
+  sensor<-sensorsall[s]
   
-  mnlssub1<-mnls %>% filter(Date>=ManageSubDateB & Date<ManageSubDate) %>% add_column(Action="Before")
-  mnlssub2<-mnls %>% filter(Date>ManageSubDate   & Date<=ManageSubDateA) %>% add_column(Action="After")
-  mnlssub<-rbind.data.frame(mnlssub1,mnlssub2)
+  data2<- data %>% 
+    filter(Sensor==sensor) %>% 
+    filter(!is.na(Value))
   
-  if(nrow(mnlssub)>1){
-    lm1<-lm(Value~Date,mnlssub1)
-    lm2<-lm(Value~Date,mnlssub2)
+  ManageDiff<-Manage[-c(13,14),]
+  
+  events<-ManageDiff$Value %>% unique %>% as.character
+  
+  lista<-list()
+  for(j in 1:nrow(ManageDiff)){
     
-    lista[[i]]<-c(lm1$coefficients[2],lm2$coefficients[2])
+    if(j==1) counter=0
     
+    ManageSub<-ManageDiff[j,]
+    subname<-as.character(ManageSub$Value)
+    date1<-as.character(ManageSub$Date)
+    
+    for(i in ndays:3){
+      
+      ManageSubDateB<-ManageSub$Date-i
+      ManageSubDate<-ManageSub$Date
+      ManageSubDateA<-ManageSub$Date+i
+      
+      mnlssub1<-data2 %>% filter(Date>=ManageSubDateB & Date<ManageSubDate) %>% add_column(Action="Before")
+      mnlssub1$Iter<-mnlssub1 %>% rownames %>% as.numeric()
+      mnlssub2<-data2 %>% filter(Date>ManageSubDate   & Date<=ManageSubDateA) %>% add_column(Action="After")
+      mnlssub2$Iter<-mnlssub2 %>% rownames %>% as.numeric()
+      mnlssub<-rbind(mnlssub1,mnlssub2)
+      
+      if(length(mnlssub1)==0|length(mnlssub2)==0) next
+      
+      if(nrow(mnlssub)>1){
+        
+        counter<-counter+1
+        
+        # Slope
+        if(nrow(mnlssub1)>1){
+          lm1<-lm(Value~Iter,mnlssub1)
+          slopelm1<-lm1$coefficients[2] 
+        } else { slopelm1=NA }
+        
+        if(nrow(mnlssub2)>1){
+        lm2<-lm(Value~Iter,mnlssub2)
+        slopelm2<-lm2$coefficients[2]
+        } else { slopelm1=NA }
+        
+        if(!is.na(coeflm1) & !is.na(coeflm2)){
+          
+          p1<-mean(diff(predict(lm1)))
+          p2<-mean(diff(predict(lm2)))
+          slopecoef<-diff(c(p1,p2))
+          
+        } else { slopecoef=NA }
+        
+        # Intercept
+        if(nrow(mnlssub1)>1){
+          lm1<-lm(Value~Iter,mnlssub1)
+          interlm1<-lm1$coefficients[1] 
+        } else { interlm1=NA }
+        
+        if(nrow(mnlssub2)>1){
+          lm2<-lm(Value~Iter,mnlssub2)
+          interlm2<-lm2$coefficients[1]
+        } else { interlm2=NA }
+        
+        if(!is.na(interlm1) & !is.na(interlm2)){
+          
+          intercoef<-diff(c(interlm1,interlm2))
+          
+        } else { intercoef=NA }
+        
+        # N
+        n.b<-nrow(mnlssub1)
+        n.a<-nrow(mnlssub2)
+        n.all<-sum(n.b,n.a)
+        
+        # Mean and Standard Deviation Before
+        if(nrow(mnlssub1)>0){
+          
+          mn.b<-mean(mnlssub1$Value)
+          sd.b<-sd(mnlssub1$Value)
+          
+        } else { mn.b<-sd.b<- NA }
+        
+        # Mean and Standard Deviation After
+        if(nrow(mnlssub2)>0){
+          
+          mn.a<-mean(mnlssub2$Value)
+          sd.a<-sd(mnlssub2$Value)
+          
+        } else { mn.a<-sd.a<- NA }
+        
+        # Mean Differences
+        if(!is.na(mn.b) & !is.na(mn.a)){
+          
+          diffmean<- diff(c(mn.b,mn.a)) %>% abs
+          
+        } else { diffmean<-NA }
+        
+        # Stdev Differences
+        if(!is.na(sd.b) & !is.na(sd.a)){
+          
+          diffsd<- diff(c(sd.b,sd.a)) %>% abs
+          
+        } else { diffsd<-NA }
+        
+        # Combination
+        lista[[counter]]<-c(
+          subname,date1,i,
+          n.b,n.a,n.all,
+          round(mn.b,4), round(mn.a,4), round(diffmean,4),
+          round(sd.b,4), round(sd.a,4), round(diffsd,4),
+          round(slopelm1,4),round(slopelm2,4), round(slopecoef,4),
+          round(interlm1,4),round(interlm2,4), round(intercoef,4))
+        
+      }
+    }
+  }
+  
+  mvals<-do.call(rbind,lista) %>% .[complete.cases(.),] %>% as.tibble
+  colnames(mvals)=c("Event","Date","Days",
+                    "NobsB","NobsA","NobsTotal",
+                    "MeanB","MeanA","MeanDiff",
+                    "SdB","SdA","SdDiff",
+                    "LMslopeB","LMslopeA","LMslopeDiff",
+                    "LMinterB","LMinterA","LMinterDiff")
+  
+  mvals<-mvals %>% mutate(SlopeMean=as.numeric(MeanDiff)*abs(as.numeric(LMslopeDiff)))
+  
+  mvallist[[s]]<-mvals
+  
+  mvals2<- mvals %>% select(Event,Date,Days,MeanDiff,LMslopeDiff,SlopeMean) %>% 
+    gather(key="Variation",value=Difference,MeanDiff,LMslopeDiff,SlopeMean) %>% 
+    mutate(Difference=as.numeric(as.character(Difference))) %>% 
+    mutate(Days=as.numeric(as.character(Days)))
+  
+  tit.short<-paste0(sensor,"_ChangeMeanNDVI_byEvent_",station)
+  tit<-paste(sensor,"Change of Mean NDVI by Management -",station)
+  g11<-ggplot(mvals2 %>% filter(Variation=="MeanDiff"),aes(Days,Difference,color=Date))+ theme_bw()+
+    geom_line()+
+    geom_point()+
+    facet_wrap(.~Event)+
+    xlab("Days Before and After Event")+
+    ggtitle(tit)+
+    scale_x_continuous(breaks=seq(0,ndays,2),limits=c(0,ndays))
+  ggsave(g11,filename = paste0(ManagementDir,station,"/",tit.short,".png"),device="png",width=12,height = 8)
+  
+  g12<-ggplot(mvals2 %>% filter(Variation=="MeanDiff"),aes(Days,Difference,group=Days))+ 
+    geom_boxplot()+theme_bw()+
+    facet_wrap(.~Event)+
+    xlab("Days Before and After Event")+
+    ggtitle(tit)+
+    scale_x_continuous(breaks=seq(0,ndays,2),limits=c(0,ndays))
+  ggsave(g12,filename = paste0(ManagementDir,station,"/",tit.short,"_box.png"),device="png",width=12,height = 8)
+  
+  tit.short<-paste0(sensor,"_ChangeLMSlope_byEvent_",station)
+  tit <- paste(sensor,"Change of Linear Regression Slope by Management -",station)
+  g21<-ggplot(mvals2 %>% filter(Variation=="LMslopeDiff"),aes(Days,Difference,color=Date))+ theme_bw()+
+    geom_line()+
+    geom_point()+
+    facet_wrap(.~Event)+
+    xlab("Days Before and After Event")+
+    ggtitle(tit)+
+    scale_x_continuous(breaks=seq(0,ndays,2),limits=c(0,ndays))
+  ggsave(g21,filename = paste0(ManagementDir,station,"/",tit.short,".png"),device="png",width=12,height = 8)
+  
+  g22<-ggplot(mvals2 %>% filter(Variation=="LMslopeDiff"),aes(Days,Difference,group=Days))+
+    geom_boxplot()+theme_bw()+
+    facet_wrap(.~Event)+
+    xlab("Days Before and After Event")+
+    ggtitle(tit)+
+    scale_x_continuous(breaks=seq(0,ndays,2),limits=c(0,ndays))
+  ggsave(g22,filename = paste0(ManagementDir,station,"/",tit.short,"_box.png"),device="png",width=12,height = 8)
+  
+  tit.short<-paste0(sensor,"_SlopeMean_byEvent_",station)
+  tit <- paste(sensor,"Difference in Slope * Mean by Management -",station)
+  g31<-ggplot(mvals2 %>% filter(Variation=="SlopeMean"),aes(Days,Difference,color=Date))+ theme_bw()+
+    geom_line()+
+    geom_point()+
+    facet_wrap(.~Event)+
+    xlab("Days Before and After Event")+
+    ylab("SlopeLM * Mean")+
+    ggtitle(tit)+
+    scale_x_continuous(breaks=seq(0,ndays,2),limits=c(0,ndays))
+  ggsave(g31,filename = paste0(ManagementDir,station,"/",tit.short,".png"),device="png",width=12,height = 8)
+  
+  
+  g32<-ggplot(mvals2 %>% filter(Variation=="SlopeMean"),aes(Days,Difference))+
+    geom_smooth(method = "lm", formula = y ~ x + I(x^2)+I(x^3), size = 1,se=F,color="grey")+
+    geom_boxplot(aes(group=Days))+theme_bw()+
+    facet_wrap(.~Event)+
+    xlab("Days Before and After Event")+
+    ylab("SlopeLM * Mean")+
+    ggtitle(tit)+
+    scale_x_continuous(breaks=seq(0,ndays,2),limits=c(0,ndays))
+  ggsave(g32,filename = paste0(ManagementDir,station,"/",tit.short,"_box.png"),device="png",width=12,height = 8)
+  
+  mvals3<-mvals2 %>% filter(Event=="Harvest")
+  mvals3$Variation<-factor(
+    mvals3$Variation,
+    labels=c("Difference in LM Slope","Difference in Mean NDVI","Difference in LM Slope * Difference in Mean NDVI"))
+  
+  tit.short<-paste0(sensor,"_SlopeMeanDiff_Harvest_",station)
+  tit <- paste(sensor,"Differences in Harvest -",station)
+  g41<-ggplot(mvals3,aes(Days,Difference))+
+    geom_boxplot(aes(group=Days)) +
+    facet_wrap(.~Variation,ncol=1,scales = "free_y") +
+    scale_x_continuous(breaks=seq(0,ndays,2),limits=c(0,ndays))+
+    theme(strip.text.x = element_text(size = 12))+
+    xlab("Days before and after")
+  ggsave(g41,filename = paste0(ManagementDir,station,"/",tit.short,"_box.png"),device="png",width=8,height = 8)
+  
+  
+  for(e in events){
+    
+    ev<- e
+    mval2<-mvals %>% select(Event,Date,Days,MeanDiff,LMslopeDiff) %>% 
+      gather(key="Variation",value=Difference,MeanDiff,LMslopeDiff) %>% 
+      mutate(Difference=as.numeric(as.character(Difference))) %>% 
+      mutate(Days=as.numeric(as.character(Days)))
+    
+    mval3<- mval2 %>% filter(Event==ev)
+    tit<-paste(sensor,"NDVI Signal Before and After",ev,"event on",station)
+    
+    if(nrow(mval3)>0){
+      
+      g1<-ggplot(mval3,aes(Days,Difference,fill=Date))+
+        geom_col(position="dodge")+
+        ggtitle(tit)+
+        ylim(c(0,.5))+
+        facet_wrap(.~Variation)+
+        scale_x_continuous(breaks=seq(2,ndays,2),limits=c(2,ndays))
+      
+      tit.short<-paste0(sensor,"_NDVI_Signal_",ev,"_",station)
+      ggsave(g1,filename = paste0(ManagementDir,station,"/",tit.short,".png"),device="png",width=12,height = 8)
+      
+    }
   }
 }
 
-mvals<-do.call(rbind.data.frame,lista) %>% .[complete.cases(.),]
-colnames(mvals)=c("Before","After")
+mvallist.bound<-do.call(rbind,mvallist) %>% as.tibble
 
+
+
+
+ggplot(mnlssub,aes(Iter,Value))+
+  geom_point()+
+  geom_smooth(data=mnlssub1,method="lm")+
+  geom_smooth(data=mnlssub2,method="lm")
 
 ggplot(mvals,aes(x=Before,y=After))+geom_point()+ylim(-.1,.1)+xlim(-.1,.1)
 
@@ -448,7 +716,6 @@ g1<-mnlssub %>%
   geom_vline(data=ManageSub,aes(xintercept=Date))+
   geom_smooth(aes(color=Action),method="lm",se=F)
 
-# glm poarameters: family = binomial,formula = y ~ poly(x,3
 
 mvals2<-mvals %>% gather
 mvals2[["Diff"]]<-c(rownames(mvals),rownames(mvals)) %>% as.numeric
