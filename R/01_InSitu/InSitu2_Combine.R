18d1e4a15eda069718d0e92903d4a2669c033458
mattia6690 <mattia.rossi@eurac.edu>
2018-09-19 17:05:55 +0200

    New in-situ combiner

--- R/01_InSitu/InSitu2_Combine.R
+++ R/01_InSitu/InSitu2_Combine.R
@@ -1,28 +1,64 @@
 ###########################'
 source("R/BaseFunctions.R")
 
+write.outputs=F
+suffix<- format.Date(Sys.Date(),"%d%m%y")
 
-# 1. Combine all the Information in one dataframe
-
-lf<-list.files(paste0(dirfield,"/03_DaySOS"),pattern=".csv",full.names=T)
-
-list1<-suppressMessages(map(lf,read_csv))
-list2<-do.call(rbind,list1) %>% select(-X1)
-
-write.csv(list2,file=paste0(MetricsDir,"/","CombinedInSituMetricsSOS.csv"))
-saveRDS(list2,file=paste0(MetricsDir,"/","CombinedInSituMetricsSOS.rds"))
-
-# 2. Aggregation of the previous result ----
-# MAKE IT TIDY
+# Long Aggregation --------------------------------------------------------
 lf<-list.files(paste0(InSitu_dir,"/07_FieldCampaign17/02_Station/"),pattern=".csv",full.names=T)
 lf_short<-list.files(paste0(InSitu_dir,"/07_FieldCampaign17/02_Station/"),pattern=".csv")
 
 stats<-do.call(rbind,str_split(lf_short,"_"))[,1]
-oi<-do.call(rbind,lapply(lf,read_csv))
-oi$Date<-as.Date(sprintf("%06s",oi$Date),"%d%m%y")
+aggr<-do.call(rbind,lapply(lf,read_csv))
+aggr$Date<-as.Date(sprintf("%06s",aggr$Date),"%d%m%y")
 
-aggr<-oi %>% group_by(FOI,Date,SubID)
-aggr2<-ungroup(aggr)
+if(write.outputs==T) write.RDSCSV(aggr,paste0(dir,"/","InSituMetrics1.csv"))
 
-write.csv(aggr2,file = paste0(MetricsDir,"/InSituMetrics.csv"))
+# Tidy Aggregation --------------------------------------------------------
+lf<-list.files(paste0(dirfield,"/03_DaySOS"),pattern=".csv",full.names=T)
+dir<-paste0(dirfield,"/","04_Combined/")
 
+list1<-suppressMessages(map(lf,read_csv))
+list2<-do.call(rbind,list1) %>% select(-X1)
+list2$Date<-as.Date(sprintf("%06s",list2$Date),"%d%m%y")
+
+if(write.outputs==T) write.RDSCSV(list2,paste0(dir,"/","InSituMetrics1_tidy.csv"))
+
+# Combination with ROI -----------------------------------------------
+list3<-list2 %>% tidyr::separate(.,OP1,c("OP1","Repetition"),sep="_") 
+
+insitu_raw<-list3 %>% 
+  filter(OP2=="NDVI" & OP3==1)  %>% 
+  group_by(Date,Station,Scale1,Scale2,OP1,OP2) %>%
+  dplyr::summarise(.,Value=mean(Value)) %>% ungroup
+
+set<-insitu_raw %>% filter(Station!="P2") %>% arrange(Station)
+
+n1<-"D,A,B,C"
+n2<-"A,B,C,D"
+phenoletters<-paste0(n1,",",
+                     paste(rep(n2,13),collapse=","),",",
+                     n1,",",
+                     paste(rep(n2,11),collapse=","))
+phenoletters<-str_split(phenoletters,",")[[1]]
+
+set2<-set %>% 
+  add_column(ROI=paste0(.$Station,"_",phenoletters)) %>% 
+  select(Date,Station,OP1,ROI)
+
+lj1<-left_join(list3,set2,by=c("Date","Station","OP1"))
+if(write.outputs==T) write.RDSCSV(lj1,paste0(dir,"/","InSituMetrics1_tidy_combined"))
+
+lj2<-left_join(insitu_raw,set2,by=c("Date","Station","OP1"))
+if(write.outputs==T) write.RDSCSV(lj2,paste0(dir,"/","InSituMetrics1_tidy_combined_ndvi"))
+
+# LAI tables --------------------------------------------------------------
+
+insitu.raw.all<-l1j %>% 
+  filter(OP3=="LAI") %>% 
+  group_by(Date,Station,OP1,OP2) %>% 
+  dplyr::summarise(meanLAI=mean(Value)) %>% 
+  filter(Station!="P2") %>% 
+  ungroup
+
+if(write.outputs==T) write.RDSCSV(lj2,paste0(Workspacedir,"07_Products/InSituLAI.rds"))
