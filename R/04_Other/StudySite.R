55e861f7a5d6c09a3c73d2dbb7f8e909c6568721
mattia6690 <mattia.rossi@eurac.edu>
2018-09-19 17:08:31 +0200

    Study Site Plot with ggPlot2

--- R/04_Other/StudySite.R
+++ R/04_Other/StudySite.R
@@ -1,61 +1,60 @@
 
 source("C:/Users/MRossi/Documents/07_Codes/PhD_Thesis/R/BaseFunctions.R")
-laodandinstall("ggmap")
+loadandinstall("ggmap")
 loadandinstall("ggsn")
 loadandinstall("broom")
+loadandinstall("sf")
+loadandinstall("purrr")
 
-stations<-readOGR(paste0(WorkspaceDir,"/01_Data/KML"),"Mattia_Stations_PhD")
-ST<-readOGR(paste0(DataDir,"/Shapes/00_General"),"SouthTyrol")
-ITA<-readOGR(paste0(DataDir,"/Shapes/00_General"),"ITA_adm0")
-
-ST2<-ST %>%broom::tidy()
-ITA2<-ITA %>% broom::tidy()
-
-stations_vi<-stations[c(1,4,5),]
-stations_do<-stations[c(2,3),]
-
-extall<-c(10,46,13,47.5)
-
-extvi<-extent(stations_vi)
-extvi[c(1,3)]<-extvi[c(1,3)]-.05
-extvi[c(2,4)]<-extvi[c(2,4)]+.05
-extvi<- as(extvi,"SpatialPolygons")
-crs(extvi)<-projection(stations_vi)
-
-extdo<-extent(stations_do)
-extdo[c(1,3)]<-extdo[c(1,3)]-.05
-extdo[c(2,4)]<-extdo[c(2,4)]+.05
-extdo<- as(extdo,"SpatialPolygons")
-crs(extdo)<-projection(stations_vi)
-
-extST<-extent(ST) %>% as.matrix %>% as.numeric
+stations<-st_read(paste0(WorkspaceDir,"/01_Data/KML"),"Mattia_Stations_PhD")[,1] %>% filter(Name!="P2")
+stations<-stations %>% 
+  mutate(Region=map(Name,function(x){
+    
+    if(grepl("Vi",x)) return("Vinschgau")
+    if(grepl("Do",x)) return("Dolomites")
+    
+  })) %>% 
+  mutate(lon=map_dbl(geometry, ~st_centroid(.x)[[1]]),lat=map_dbl(geometry, ~st_centroid(.x)[[2]]))
 
 
-mymap<-get_map(location=extall,source="google",maptype="satellite")
+
+ST<-readOGR(paste0(DataDir,"/Shapes/00_General"),"SouthTyrol") %>% st_as_sf()
+ITA<-readOGR(paste0(DataDir,"/Shapes/00_General"),"ITA_adm0") %>% st_as_sf()
+
+
+extvi<-stations %>% filter(Region=="Vinschgau") %>% st_bbox %>% st_as_sfc %>% st_buffer(dist=.1)
+extdo<-stations %>% filter(Region=="Dolomites") %>% st_bbox %>% st_as_sfc %>% st_buffer(dist=.1)
+
+
+
+extST1<-ST %>% extent %>% as.matrix %>% as.numeric
+extST2<-ST %>% st_buffer(dist=.5) %>% extent %>% as.matrix %>% as.numeric
+
+mymap<-get_map(location=extST2,source="google",maptype="satellite")
 coords<-cbind(stations@coords[,1],stations@coords[,2]) %>% as.data.frame()
 
 # Plot the Study Site with the Location Bouundaries divided in Dolomites and Vinschgau Valley
 g1<-ggmap(mymap)+
-  geom_polygon(data=ST2,aes(x=long,y=lat,group=group),fill="gray54",color="gray54",size=1.5,alpha=.3)+
-  geom_polygon(aes(x=long,y=lat,group=id,color="Vinschgau"),data=extvi,fill=NA,size=1.2)+
-  geom_polygon(aes(x=long,y=lat,group=id,color="Dolomites"),data=extdo,fill=NA,size=1.2)+
-  geom_point(aes(x=V1,y=V2,color="Vinschgau"),data=coords[c(1,4,5),],size=2)+
-  geom_point(aes(x=V1,y=V2,color="Dolomites"),data=coords[c(2,3),],size=2)+
+  geom_sf(data=ST,inherit.aes = FALSE,fill="darkolivegreen3",color="darkolivegreen2",size=1.5,alpha=.2)+
+  geom_sf(data=stations,inherit.aes = F,cex=4,pch=17)+
+  geom_sf(data=extvi,inherit.aes = F,fill=NA,size=1.2,color="red")+
+  geom_sf(data=extdo,inherit.aes = F,fill=NA,size=1.2,color="deepskyblue")+
+  geom_label(data=stations, aes(x=lon, y=lat, label=Name),hjust = 0, nudge_x = 0.05,size=10) +
   xlab("Longitude")+ylab("Latitude")+
-  scale_color_manual("Site Location",values=c("green","red"))+
-  coord_map(xlim=c(extST[1],extST[3]),ylim=c(extST[2],extST[4]))
-
-ggsave(g1,filename = paste0(DataDir,"Images/StudySites.png"),device = "png",height=8,width=11)
+  coord_sf(xlim=c(extST1[1],extST1[3]),ylim=c(extST1[2],extST1[4]))+
+  theme(legend.position="none")
+  
+ggsave(g1,filename = paste0(DataDir,"Images/StudySites_gg.png"),device = "png",height=9,width=16)
 
 eurac.color<-rgb(208,77,31,maxColorValue = 255)
 plot(ITA,border="mediumseagreen")
 plot(ST,add=TRUE,col=eurac.color,border="mediumseagreen")
 
 # Italy and South Tyrol Plot
-g2<-ggplot()+theme_minimal()+
-  geom_polygon(data=ITA2,aes(x=long,y=lat,group=group),fill="lightgoldenrod")+
-  geom_polygon(data=ST2,aes(x=long,y=lat,group=group),fill="lightcoral")+
-  xlab("Longitude")+ylab("Latitude")
+g2<-ggplot()+theme_light()+
+  geom_sf(data=ITA,inherit.aes = F)+
+  geom_sf(data=ST,inherit.aes = F,fill="darkolivegreen2")+
+  geom_sf(data=extvi,inherit.aes = F,fill=NA,size=1.2,color="red")+
+  geom_sf(data=extdo,inherit.aes = F,fill=NA,size=1.2,color="deepskyblue")
 
-ggsave(g2,filename = paste0(DataDir,"Images/ItalyST.png"),device = "png",height=6,width=5)
-               
+ggsave(g2,filename = paste0(DataDir,"Images/ItalyST_gg.png"),device = "png",height=12,width=8)
