3f047808564be73dcacda884b5227e7defbcbf8e
Mattia <mattia.rossi@eurac.edu>
2018-09-20 18:40:02 +0200

    Added r2 equation as string function

--- R/BaseFunctions.R
+++ R/BaseFunctions.R
@@ -116,6 +116,16 @@ write.RDSCSV<-function(file,name){
   
 }
 
+# Transforms an lm function output to text usable for ggplots
+# Greetings to Jodie Burchell (http://t-redactyl.io/)
+r2.equation = function(x) {
+  lm_coef <- list(a = round(coef(x)[1], digits = 2),
+                  b = round(coef(x)[2], digits = 2),
+                  r2 = round(summary(x)$r.squared, digits = 2));
+  lm_eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(R)^2~"="~r2,lm_coef)
+  as.character(as.expression(lm_eq));                 
+}
+
 
 #* 4.2. InSitu Functions ----
 
